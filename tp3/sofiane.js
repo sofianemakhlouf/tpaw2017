f9044e1660bc553c0ddb647789d902f4f97bd8f8
sofianemakhlouf <makhlouf.sofiane@hotmail.fr>
2016-10-21 16:54:18 +0200

    Add files via upload

--- tp3/sofiane.js
+++ tp3/sofiane.js
@@ -1,16 +1,18 @@
-$(document).ready(function(){
- 
- ("#submitForm").on('click',function(){
-  if(§("#inputEmail3").val()==""){
-   §('.modal-body').html('veuillez remplir les champs svp');
-  }
-  else{
-   §('.modal-body').html('fff');
-   §('.modal').modal('show'); 
-  }
- })
-});
- 
- 
+            
+ $(document).ready(function(){
 
+ 	$("#bouton").on('click',function(){
+ 		if($("#Name").val()==""){
+ 			$('.modal-body').html('veuillez remplir les champs svp');
+ 			$('.modal').modal('show');
 
+ 		}
+ 		else {
+
+ 		$('.modal-body').html('<p> vous êtes nés le 21/112/2000 et vous habitez <a href="https://developers.google.com/maps/documentation/static-maps"><img src="paris.png" </a></p> ');
+ 		$('.modal').modal('show');
+ 	}
+ 	});
+
+ 	
+	});
