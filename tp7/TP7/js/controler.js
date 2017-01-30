f5875279d8a8aa6a6b5fd68d795f022a21762ab7
sofianemakhlouf <makhlouf.sofiane@hotmail.fr>
2017-01-30 21:35:00 +0100

    Add files via upload

---
+++ tp7/TP7/js/controler.js
@@ -0,0 +1,74 @@
+var meteoControllers = angular.module('meteoControllers', []);
+
+meteoControllers.controller('MainController', ['$scope', '$http',
+   function($scope, $http) {
+
+	$scope.recherche = function() {
+        $scope.errorMsg =  "";
+        /* appel AJAX à l’API openweathermap */	    
+$http.get('https://demo.bilelz.fr/owmap/?q='+$scope.city+'&units=metric&appid=0ada432b59deb9716c357092c5f79be6') /* mettre votre API id */
+	    	.success(function(data){ 
+	    		/* on met dans l’objet meteo les données retournées par openweathermap */
+	    		 $scope.meteo = data;
+                 if(data == ""){
+                    $scope.errorMsg =  "Pas de résultat pour votre recherche..."; 
+                 }
+	    	 }).error(function(data) {
+	    		/* en cas d’erreur */
+	    		 $scope.errorMsg =  "Hum. Error... please retry.";
+	    	 });
+
+			$http.get('https://demo.bilelz.fr/owmap/forecast/?q='+$scope.city+'&'+'&units=metric&appid=0ada432b59deb9716c357092c5f79be6'+'&lang=fr')
+				.success(function(data) {
+					$scope.forecast = data;
+				console.log($scope.forecast) ;
+				}).error(function(data) {
+					$scope.loadingMsg = "Erreur pour les données sur 5 jours...";
+				});
+            
+	}
+
+	$scope.gps = function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else { 
+            $scope.errorMsg = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    function showPosition(position) {
+        $scope.errorMsg =  "";
+        /* appel AJAX à l’API openweathermap */	    
+        $http.get('https://demo.bilelz.fr/owmap/?lat='+position.coords.latitude+'&lon='+position.coords.longitude+'&units=metric&appid=0ada432b59deb9716c357092c5f79be6') /* mettre votre API id */
+            .success(function(data) {
+                /* on met dans l’objet meteo les données retournées par openweathermap */
+                $scope.meteo = data;
+                if(data == ""){
+                $scope.errorMsg =  "Pas de résultat pour votre recherche..."; 
+                }
+            }).error(function(data) {
+                /* en cas d’erreur */
+                $scope.errorMsg =  "Hum. Error... please retry.";
+            });
+    }
+
+	
+	
+	
+    function showError(error) {
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                $scope.errorMsg  = "User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                $scope.errorMsg  = "Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                $scope.errorMsg  = "The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                $scope.errorMsg  = "An unknown error occurred."
+                break;
+        }
+    }     
+}]);
